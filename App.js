6eadbdc7b12908ffaaaa709ca72fa5949ee34f34
Dolores Cueto <cuetodolores@gmail.com>
2021-10-19 21:11:27 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { NavBar } from './components/Navbar';
+import { CartWidget } from './components/CartWidget';
+
+function App() {
+  return (
+    
+    <div className="App">
+
+      <NavBar />
+  	  <CartWidget />
+      <header className="App-header">
+      <p>Prueba</p>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
